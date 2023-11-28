9c1062ba9238c4ebd0f281a6f143a3eb5c749e97
maydayv7 <maydayv7@gmail.com>
2023-11-28 21:03:53 +0530

    feat: Add wallpaper

--- flake.nix
+++ flake.nix
@@ -4,27 +4,24 @@
     utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = {
-    self,
-    nixpkgs,
-    utils,
-  }: let
-    inherit (nixpkgs) lib;
-  in
-    utils.lib.eachDefaultSystem (system: let
-      pkgs = nixpkgs.legacyPackages.${system};
-    in {
-      packages.fonts = pkgs.callPackage ./fonts {inherit lib pkgs;};
-    })
-    // {
-      files = {
-        discord = ./discord;
-        wallpapers = with lib; let
-          path = ./wallpapers;
-        in
-          (lib.mapAttrs' (name: _: lib.nameValuePair (lib.removeSuffix ".jpg" name) "${path}/${name}")
-            (builtins.readDir path))
-          // {inherit path;};
+  outputs = inputs:
+    with inputs; let
+      inherit (nixpkgs) lib;
+    in
+      utils.lib.eachDefaultSystem (system: let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        packages.fonts = pkgs.callPackage ./fonts {inherit lib pkgs;};
+      })
+      // {
+        files = {
+          discord = ./discord;
+          wallpapers = let
+            path = ./wallpapers;
+          in
+            (lib.mapAttrs' (name: _: lib.nameValuePair (lib.removeSuffix ".jpg" name) "${path}/${name}")
+              (builtins.readDir path))
+            // {inherit path;};
+        };
       };
-    };
 }
