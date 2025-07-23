182c2803995962c7e3ea5e491ae4294325766e59
maydayv7 <maydayv7@gmail.com>
2025-07-23 17:12:55 +0530

    feat: Remove Shaders
    Also update flake

--- flake.nix
+++ flake.nix
@@ -4,31 +4,42 @@
     utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = inputs:
-    with inputs; let
+  outputs =
+    inputs:
+    with inputs;
+    let
       inherit (nixpkgs) lib;
     in
-      utils.lib.eachDefaultSystem (system: let
+    utils.lib.eachDefaultSystem (
+      system:
+      let
         pkgs = nixpkgs.legacyPackages.${system};
-        call = path: pkgs.callPackage path {inherit lib pkgs;};
-      in {
+        call = path: pkgs.callPackage path { inherit lib pkgs; };
+      in
+      {
         packages = {
           fonts = call ./fonts;
           cursors = call ./cursors;
         };
-      })
-      // {
-        files = let
-          call = path: ext:
-            (lib.filterAttrs (_: val:
-              !(lib.hasSuffix ".nix" val)) (lib.mapAttrs' (name: _:
-              lib.nameValuePair (lib.removeSuffix ext name) "${path}/${name}")
-            (builtins.readDir path)))
-            // {inherit path;};
-        in {
+      }
+    )
+    // {
+      files =
+        let
+          call =
+            path: ext:
+            (lib.filterAttrs (_: val: !(lib.hasSuffix ".nix" val)) (
+              lib.mapAttrs' (name: _: lib.nameValuePair (lib.removeSuffix ext name) "${path}/${name}") (
+                builtins.readDir path
+              )
+            ))
+            // {
+              inherit path;
+            };
+        in
+        {
           fonts = call ./fonts ""; # Proprietary Fonts
-          shaders = call ./shaders ".frag"; # Hyprland Shaders
           wallpapers = call ./wallpapers ".jpg"; # Beautiful Wallpapers
         };
-      };
+    };
 }
--- shaders/dim.frag
+++
@@ -1,9 +0,0 @@
-// Screen Dimming
-
-precision highp float;
-varying vec2 v_texcoord;
-uniform sampler2D tex;
-void main()
-{
-  gl_FragColor = texture2D(tex, v_texcoord) * 0.3;
-}
